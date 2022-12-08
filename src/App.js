9972637a9a258ee0090451861fd8ccf90e2abb37
shailesh1694 <morishailesh982@gmail.com>
2022-12-08 09:23:01 +0530

    finalproject

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Cart from "./Components/Cart";
+import Search from "./Components/Search";
+import React, { useReducer, createContext, useEffect } from "react";
+import { reducer } from "./Components/reducer";
+import API from "./Components/API";
+import Total from "./Components/Total";
+
+
+//initial value for reducer hook
+
+const initialvalue = {
+  item: API,
+  isloadingd: false,
+  toggle:true,
+  totalQuantity:0,
+  totalAmount:0
+};
+//create contex for data pass in components
+export const Data = createContext();
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialvalue);
+
+
+  //for remove item in card
+  const removeitem = (id) => {
+    console.log(id);
+   return dispatch({ type: "REMOVE", payload: id });
+  };
+
+
+  //Claar Card
+  const clearall = () => {
+   return dispatch({ type: "CLEAR" });
+  };
+
+  //increment 
+  const increment=(id)=>{
+   return dispatch({type:"INCREMENT",payload:id})
+
+  }
+  //decrement 
+  const decrement =(id)=>{
+   return dispatch({type:"DECREMENT",payload:id
+    })
+  }
+
+
+  // for total data
+  useEffect(() => {
+    dispatch({
+      type: "GET_TOTAL",
+    });
+  }, [state.item]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Data.Provider value={{ ...state, removeitem, clearall,increment,decrement }}>
+        <Search />
+        <br></br>
+        <h1>{`Total card Itme is ${state.totalQuantity}`} </h1>
+        <Cart />
+      { state.toggle? <Total />:<h4>No Itme in Card !</h4>}
+      </Data.Provider>
     </div>
   );
 }
